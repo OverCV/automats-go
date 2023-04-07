20dd1f6836bc30b4b2f67d6fcfa4f13850d9cca9
vogel <over.castrillon22396@ucaldas.edu.co>
2023-04-07 14:25:12 -0500

    Adition of Json functionality

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.20
 
 require (
 	github.com/awalterschulze/gographviz v2.0.3+incompatible // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/goccy/go-gographviz v0.1.1 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	go.mongodb.org/mongo-driver v1.11.4 // indirect
+	golang.org/x/image v0.6.0 // indirect
 )
--- models/state.go
+++ models/state.go
@@ -1,17 +1,23 @@
 package models
 
 import (
-	"fmt"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strings"
 )
 
 type State struct {
-	Id        primitive.ObjectID `json:"_id" bson:"_id"`
 	Data      string
 	IsInitial bool
 	IsFinal   bool
 	Adjacent  []string
+}
+
+func NewState(data string) *State {
+	return &State{
+		Data:      data,
+		IsInitial: false,
+		IsFinal:   false,
+		Adjacent:  []string{},
+	}
 }
 
 func (s *State) SetData(data string) {
@@ -20,14 +26,6 @@ func (s *State) SetData(data string) {
 
 func (s *State) GetData() string {
 	return s.Data
-}
-
-func (s *State) GetAdjacent() []string {
-	return s.Adjacent
-}
-
-func (s *State) SetAdjacent(adjacent []string) {
-	s.Adjacent = adjacent
 }
 
 func (s *State) SetIsInitial(initial bool) {
@@ -46,16 +44,19 @@ func (s *State) GetIsFinal() bool {
 	return s.IsFinal
 }
 
+func (s *State) GetAdjacent() []string {
+	return s.Adjacent
+}
+
+func (s *State) SetAdjacent(adjacent []string) {
+	s.Adjacent = adjacent
+}
+
 func (s *State) ToString() string {
-	brackets := ""
-	if len(s.Adjacent) == 0 {
-		brackets = "[ ]"
-	} else {
-		brackets = fmt.Sprintf("[ %v ]", s.Adjacent)
-	}
-	data := fmt.Sprintf("%s | %s", s.Data, brackets)
+	brackets := "[ " + strings.Join(s.Adjacent, ", ") + " ]"
+	data := s.Data + " | " + brackets
 	if s.IsFinal {
-		return fmt.Sprintf("((%s))", data)
+		return "((" + data + "))"
 	}
-	return fmt.Sprintf("(%s)", data)
+	return "(" + data + ")"
 }
